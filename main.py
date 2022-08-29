e259ff2e52e915570d50f648a6958b114375db58
Deutschkiller <dl177180@hotmail.com>
2022-08-29 14:02:30 +0800

    Main

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from typing_extensions import Self
+# from typing_extensions import Self
 
 
 def Instance():
@@ -35,40 +35,63 @@ def Instance():
 class Module():
     def __init__(self,name) -> None:
         self.name = name
-        self.input = []
-        self.output = []
-        self.parameter = []
+        self.interfaces = []
 
-    def add_input(self,input):
-        self.input.append(input)
-    
-    def remove_input(self,input):
-        self.input.remove(input)
+    def add_if(self,if_type,name):
+        self.interfaces.append(Interface(if_type,name))
 
-    def add_output(self,output):
-        self.output.append(output)
-    
-    def remove_output(self,output):
-        self.output.remove(output)
+    def remove_if(self,if_type,name):
+        self.interfaces.remove(Interface(if_type,name))
 
-    def add_parameter(self,parameter):
-        self.parameter.append(parameter)
-    
-    def remove_parameter(self,parameter):
-        self.parameter.remove(parameter)
+    def show_interfaces(self):
+        for e in self.interfaces:
+            print(e.if_type+" "+str(e.width)+" "+e.name)
+
+    def print_info(self):
+        print("-----------------")
+        print("module name:" + self.name)
+        self.show_interfaces()
+        print("-----------------")
+class Interface():
+    def __init__(self,if_type,name) -> None:
+        self.width = 1
+        self.name = name
+        self.if_type = if_type
 
 class Verilog_file():
-    def __init__(self,path) -> None:
+    def __init__(self,path,verilog_type) -> None:
         self.path = path
         self.load_file()
+        self.verilog_type = verilog_type
+        if(self.verilog_type == "1995"):
+            self.parse_module_1995()
     
     def load_file(self):
         with open(self.path,"r") as f:
             self.content = f.readlines()
         return self.content
     
-    def detect_module(self):
+    def parse_module_1995(self): #分析1995格式verilog 分析模块信息，不分析梨花信息
         for line in self.content:
+            if line[0:6] == "module":
+                self.module = Module(line[6:])
+            if line[0:5] == "input":
+                self.module.add_if("input",line[5:-1])
+            if line[0:6] == "output":
+                self.module.add_if("output",line[6:-1])
+            if line[0:4] == "wire":
+                self.module.add_if("wire",line[4:-1])
+            if line[0:3] == "reg":
+                self.module.add_if("wire",line[3:-1])
+
+   
+
             
     def detect_instance(self):
         pass
+
+test_verilog = Verilog_file("./test_verilogs/arp.v","1995")
+
+print(test_verilog.module.interfaces)
+
+test_verilog.module.print_info()
